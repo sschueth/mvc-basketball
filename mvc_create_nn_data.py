235bdc3f510c2f50f7c055cd2abc05683be24680
sschueth <sschueth96@gmail.com>
2019-12-20 00:04:55 -0600

    Created nn training data (inputs to a NN).

---
+++ mvc_create_nn_data.py
@@ -0,0 +1,89 @@
+import numpy as np
+import pickle
+import datetime
+
+year = '2017-18'
+
+pickle_db = 'databases/box-score-dict-'+year+'.p'
+with open(pickle_db,'rb') as pick_file:
+    data_db = pickle.load(pick_file)
+
+with open(year+'/conference teams.txt','r') as conf_teams_file:
+    conf_team_file = conf_teams_file.readlines()
+
+conf_teams = []
+
+box_score_fields = ['FG','FGA','3FG','3FGA','FT','FTA','ORB','DRB','REB','PF','TP','A','TO','BLK','S']
+
+
+
+for c in conf_team_file:
+    if '\n' in c:
+        c = c.replace('\n','')
+    conf_teams.append(c)
+data_avg_db = {}
+for team in conf_teams:
+    data_avg_db[team] = {}
+    this_team_idx_list = []
+    this_date_list = []
+    this_team_home_list = []
+    for idx in range(0,len(data_db)):
+        if team in data_db[idx]['Home']['Team']:
+            this_team_idx_list.append(idx)
+            this_date_list.append(datetime.date(month = int(data_db[idx]['Month']), day = int(data_db[idx]['Day']), year = int(data_db[idx]['Year'])))
+            this_team_home_list.append(1)
+        if team in data_db[idx]['Visitor']['Team']:
+            this_team_idx_list.append(idx)
+            this_date_list.append(datetime.date(month = int(data_db[idx]['Month']), day = int(data_db[idx]['Day']), year = int(data_db[idx]['Year'])))
+            this_team_home_list.append(0)
+
+    while len(this_team_idx_list) > 0:
+        max_date = this_date_list[0]
+        max_date_idx = 0
+        for j in range(0,len(this_date_list)):
+            if this_date_list[j] > max_date:
+                max_date = this_date_list[j]
+                max_date_idx = j
+        
+        data_avg_db[team][max_date] = {}
+        data_avg_db[team][max_date]['For'] = {}
+        data_avg_db[team][max_date]['Against'] = {}
+        for i in range(0,len(box_score_fields)):
+            data_avg_db[team][max_date]['For'][box_score_fields[i]] = 0
+            data_avg_db[team][max_date]['Against'][box_score_fields[i]] = 0
+        for i in range(0,len(this_team_idx_list)):
+            if this_team_home_list[i] == 1:
+                for field in box_score_fields:
+                    data_avg_db[team][max_date]['For'][field] = data_avg_db[team][max_date]['For'][field] + data_db[this_team_idx_list[i]]['Home'][field]
+                    data_avg_db[team][max_date]['Against'][field] = data_avg_db[team][max_date]['Against'][field] + data_db[this_team_idx_list[i]]['Visitor'][field]
+            else:
+                for field in box_score_fields:   
+                    data_avg_db[team][max_date]['For'][field] = data_avg_db[team][max_date]['For'][field] + data_db[this_team_idx_list[i]]['Visitor'][field]
+                    data_avg_db[team][max_date]['Against'][field] = data_avg_db[team][max_date]['Against'][field] + data_db[this_team_idx_list[i]]['Home'][field]
+        
+        data_avg_db[team][max_date]['For']['eFG%'] = ( data_avg_db[team][max_date]['For']['FG'] + 0.5*data_avg_db[team][max_date]['For']['3FG'] )/ data_avg_db[team][max_date]['For']['FGA']
+        data_avg_db[team][max_date]['For']['TOV%'] = data_avg_db[team][max_date]['For']['TO'] / (data_avg_db[team][max_date]['For']['FGA'] + 0.44*data_avg_db[team][max_date]['For']['FTA'] + data_avg_db[team][max_date]['For']['TO'])
+        data_avg_db[team][max_date]['For']['ORB%'] = data_avg_db[team][max_date]['For']['ORB'] / (data_avg_db[team][max_date]['Against']['DRB'] + data_avg_db[team][max_date]['For']['ORB'])
+        data_avg_db[team][max_date]['For']['DRB%'] = data_avg_db[team][max_date]['For']['DRB'] / (data_avg_db[team][max_date]['For']['DRB'] + data_avg_db[team][max_date]['Against']['ORB'])
+        data_avg_db[team][max_date]['For']['FTf'] = data_avg_db[team][max_date]['For']['FT']/data_avg_db[team][max_date]['For']['FGA']
+
+        data_avg_db[team][max_date]['Against']['eFG%'] = ( data_avg_db[team][max_date]['Against']['FG'] + 0.5*data_avg_db[team][max_date]['Against']['3FG'] )/ data_avg_db[team][max_date]['Against']['FGA']
+        data_avg_db[team][max_date]['Against']['TOV%'] = data_avg_db[team][max_date]['Against']['TO'] / (data_avg_db[team][max_date]['Against']['FGA'] + 0.44*data_avg_db[team][max_date]['Against']['FTA'] + data_avg_db[team][max_date]['Against']['TO'])
+        data_avg_db[team][max_date]['Against']['ORB%'] = data_avg_db[team][max_date]['Against']['ORB'] / (data_avg_db[team][max_date]['For']['DRB'] + data_avg_db[team][max_date]['Against']['ORB'])
+        data_avg_db[team][max_date]['Against']['DRB%'] = data_avg_db[team][max_date]['Against']['DRB'] / (data_avg_db[team][max_date]['Against']['DRB'] + data_avg_db[team][max_date]['For']['ORB'])
+        data_avg_db[team][max_date]['Against']['FTf'] = data_avg_db[team][max_date]['Against']['FT']/data_avg_db[team][max_date]['Against']['FGA']
+
+
+        this_team_idx_list.pop(max_date_idx)
+        this_date_list.pop(max_date_idx)
+        this_team_home_list.pop(max_date_idx)
+        #print(data_avg_db[team][max_date])
+        #print(max_date)
+
+pickle_name = 'nn-training-data-inputs-'+year+'.p'         
+with open(pickle_name,'wb') as pick_file:
+    pickle.dump(data_avg_db, pick_file)
+
+print('Saved dictionary: ' + pickle_name)
+
+#print(data_avg_db)
